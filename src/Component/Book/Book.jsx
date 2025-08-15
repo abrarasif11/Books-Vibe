120d5ee323033124157cca192e8865e6db57e361
abrarasif11 <faasif929@gmail.com>
2025-08-15 22:15:31 +0600

    card-responsive

--- src/Component/Book/Book.jsx
+++ src/Component/Book/Book.jsx
@@ -1,26 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Book = ({book}) => {
+const Book = ({ book }) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src={book.image}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      Card Title
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+    <div className="card bg-base-100 w-96 p-6 shadow-sm">
+      <figure className="bg-[#F3F3F3] p-8 rounded-2xl">
+        <img src={book.image} alt={book.bookName} className="h-[166px]" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{book.bookName}</h2>
+        <p>By : {book.author}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default Book
+export default Book;
