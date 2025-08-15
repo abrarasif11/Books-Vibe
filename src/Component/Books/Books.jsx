418c484ee7896bea4d6dbec26e6b5811e39d7dfd
abrarasif11 <faasif929@gmail.com>
2025-08-15 22:02:03 +0600

    added-books-data

--- src/Component/Books/Books.jsx
+++ src/Component/Books/Books.jsx
@@ -10,9 +10,9 @@ const Books = () => {
   }, []);
   return (
     <div>
-      <h2 className="text-4xl-font-bold-text-center">Books</h2>
-      {books.length}
-      <div>
+      <h2 className="text-4xl mb-10  font-bold text-center">Books</h2>
+      
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         {books.map((book) => (
           <Book book={book} key={book.bookId} />
         ))}
