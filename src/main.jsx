489de26f55e3d53771ddbab07437cd1111e0072b
abrarasif11 <faasif929@gmail.com>
2025-08-15 19:42:37 +0600

    footer-added

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Layout from './Component/Layout/Layout.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Layout/>
   },
 ]);
 
