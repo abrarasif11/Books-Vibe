4855af6848f82cd8e8d1de1298e7b8f844d858d0
abrarasif11 <faasif929@gmail.com>
2025-08-16 15:14:07 +0600

    marked-as-read-local-storage

--- src/Component/BookDetail/BookDetail.jsx
+++ src/Component/BookDetail/BookDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../Utility/AddToDb";
 
 const BookDetail = () => {
   const { bookId } = useParams();
@@ -7,6 +8,9 @@ const BookDetail = () => {
   const id = parseInt(bookId);
   const book = data.find((book) => book.bookId === id);
   const { image,author,bookName,category,review } = book;
+  const handleMarkAsRead = (id) => {
+    addToStoredReadList(id);
+  }
   return (
     <div className="hero bg-[#F3F3F3] mx-auto mt-10 mb-10">
       <div className="hero-content flex-col lg:flex-row">
@@ -28,8 +32,8 @@ const BookDetail = () => {
           <div className="border-t-2 border-dashed mb-5"></div>
           <p>Review : {review}</p>
           <div className="flex mt-10 ">
-          <button className="btn btn-outline mr-10 p-3 rounded-lg">Read</button>
-          <button className="btn bg-[#50B1C9] text-white rounded-lg">Wishlist</button>
+          <button onClick={()=> handleMarkAsRead(bookId)} className="btn btn-outline mr-10 p-3 rounded-lg">Mark As Read</button>
+          <button className="btn bg-[#50B1C9] text-white rounded-lg">Add to Wishlist</button>
           </div>
         </div>
       </div>
